42d62962362fa75fc8872ae8bd3524707b05eba5
MG Song <mg-song@poongsan.co.kr>
2021-01-12 16:34:59 +0900

    사용자 함수를 이용한 HelloWorld

--- KoreaITAcademy/HelloWorld/HelloWorld.c
+++ KoreaITAcademy/HelloWorld/HelloWorld.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+//void printByUser(char *pString);
+
+void printByUser(char* pString)
+{
+	printf("%s\n", pString);
+}
+
 int main(void)
 {
-	printf("Hello World!\n");
+	char strHello[256] = "Hello World";
+
+	printByUser(strHello);
 	return 0;
 }
+
+//void printByUser(char *pString)
+//{
+//	printf("%s\n", pString);
+//}
